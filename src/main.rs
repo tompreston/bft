568d6a941296c4e083dac5dab47f562037223f4f
Thomas Preston <thomas.preston@codethink.co.uk>
2020-05-23 22:33:14 +0100

    main: Add a TidyWriter for ensuring neat new-lines

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,46 @@ use bft_interp::BrainfuckVM;
 use bft_types::BrainfuckProg;
 use std::error::Error;
 use std::io;
+use std::ops::Drop;
 
 mod cli;
 use cli::{BrainfuckOpt, StructOpt};
+
+/// Proxy writer which ensures the last byte written is a new-line.
+struct TidyWriter<T> {
+    writer: T,
+
+    /// Keep track of the last byte.
+    last_u8: u8,
+}
+
+impl<T> io::Write for TidyWriter<T>
+where
+    T: io::Write,
+{
+    /// Proxy write command which tracks the last byte.
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match buf.last() {
+            Some(l) => self.last_u8 = *l,
+            None => (),
+        }
+        self.writer.write(buf)
+    }
+
+    /// Proxy flush command.
+    fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush()
+    }
+}
+
+impl<T> Drop for TidyWriter<T> {
+    /// When the TidyWriter is dropped, ensure there is a new line.
+    fn drop(&mut self) {
+        if self.last_u8 != b"\n"[0] {
+            println!()
+        }
+    }
+}
 
 fn run_bft(opt: BrainfuckOpt) -> Result<(), Box<dyn Error>> {
     let num_cells = match opt.cells {
@@ -15,7 +52,11 @@ fn run_bft(opt: BrainfuckOpt) -> Result<(), Box<dyn Error>> {
     bf_prog.check()?;
 
     let mut bf_vm: BrainfuckVM<u8> = BrainfuckVM::new(&bf_prog, num_cells, opt.extensible);
-    bf_vm.interpret(io::stdin(), io::stdout())?;
+    let twriter = TidyWriter {
+        writer: io::stdout(),
+        last_u8: 0,
+    };
+    bf_vm.interpret(io::stdin(), twriter)?;
     Ok(())
 }
 
